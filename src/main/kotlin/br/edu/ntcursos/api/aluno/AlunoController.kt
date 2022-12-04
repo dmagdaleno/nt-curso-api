0f07335860d73fa76c73150ac74a65ad4a602f9d
dmagdaleno <diegommagdaleno@gmail.com>
2022-12-04 02:03:21 -0300

    corrige problema de CORS

--- src/main/kotlin/br/edu/ntcursos/api/aluno/AlunoController.kt
+++ src/main/kotlin/br/edu/ntcursos/api/aluno/AlunoController.kt
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/alunos")
+@CrossOrigin(origins = ["*"])
 class AlunoController(
     val repositorio: AlunoRepository
 ) {
